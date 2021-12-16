1966c5889e7c0ac794c9e678601a2662517a1c1a
nicolasbuchwalder <okkin9991@gmail.com>
2021-12-17 00:25:36 +0100

    fix cell stuck in borders

--- src/rules/cell_rules.py
+++ src/rules/cell_rules.py
@@ -1,7 +1,12 @@
+import numpy as np
+
 def cell_rule1(self, clock, pos_map):
-    self.move()
+    move = (self.actions[:2].ravel() - self.actions[2:4].ravel()) * self.velocity
+    self.pos += move - (move + self.pos > (self.grid_size-1)) * 2*((move + self.pos) - self.grid_size+1) - 2*(move + self.pos < 0) * ((move + self.pos))
     self.update_sensors(clock, pos_map)
     self.think()
     self.age += 1
     if self.age >= self.death_age:
         self.alive = False
+
+#self.pos = np.maximum.reduce([np.minimum.reduce([self.pos + (self.actions[:2].ravel() - self.actions[2:4].ravel()) * self.velocity, np.ones(2)*(self.grid_size-1)]), np.zeros(2)])
