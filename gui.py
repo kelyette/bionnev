8de3c25d83ede55ddfd31e3a3e820b49887a4ab4
Nicolas Buchwalder <nicolas.buchwalder@epfl.ch>
2021-12-16 14:27:53 +0100

    add yaml files for env and sim rules

--- gui.py
+++ gui.py
@@ -1,39 +1,128 @@
+import yaml
+from PySimpleGUI.PySimpleGUI import SELECT_MODE_SINGLE
 import numpy as np
 import PySimpleGUI as sg
-import matplotlib, time, threading
+import matplotlib
+import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-sg.theme('SystemDefault')
+with open("params.yaml", "r") as f:
+    params = yaml.safe_load(f)["one"]
+# with open("env_rules.yaml", "r") as f:
+#     env_rules = yaml.safe_load(f)    
+# with open("cell_rules.yaml", "r") as f:
+#     cell_rules = yaml.safe_load(f)
+
+sg.theme('SystemDefault1')
 font = ('Helvetica', 12)
 titlefont = ('Helvetica', 14, "bold")
+figsize = (5,5)
 
+def plot(fig, map):
+    if fig is not None:
+        fig.get_tk_widget().forget()
+    fig = matplotlib.figure.Figure(figsize=figsize)
+    fig.add_subplot(111)
+    if map is None:
+        return fig
+    
 def draw_figure(canvas, figure, loc=(0, 0)):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
     return figure_canvas_agg
 
+def choose_rule(rule_dict, chosen_rule, title):
+    rule_left_layout = [
+        [sg.Text("All available rules",font=titlefont)],
+        [sg.Listbox([rule for rule in rule_dict.keys()], default_values=chosen_rule, key='selected_rule', select_mode=SELECT_MODE_SINGLE, size=(10,10), enable_events=True)],
+    ]
+    rule_right_layout = [
+        [sg.Text("Explanation",font=titlefont)],
+        [sg.Multiline(rule_dict[chosen_rule],size=(40,8),key='explanation')],
+        [sg.Text(" ",size=(35,1)), sg.Button('Choose',enable_events=True)]
+    ]
+    rule_layout = [
+        [sg.Column(rule_left_layout), sg.Column(rule_right_layout)]
+    ]
+    rule_window = sg.Window(title,rule_layout, font=font)
+    while True: 
+        rule_event, rule_values = rule_window.read()
+        if rule_event == sg.WIN_CLOSED:
+            break
+        elif rule_event == 'Choose':
+            chosen_rule = rule_values['selected_rule'][0]
+            break
+        elif rule_values['selected_rule'][0] != chosen_rule:
+            chosen_rule = rule_values['selected_rule'][0]
+            rule_window.Element('explanation').Update(value=rule_dict[chosen_rule])
+    rule_window.close()
+
+    return chosen_rule
+
+# def change_params(params, env_rule, cell_rule):
+    
+#     params_left_layout = [
+#         [sg.Text("All available parameters",font=titlefont)],
+#         [sg.Listbox([rule for rule in rule_dict.keys()], default_values=chosen_rule, key='selected_rule', select_mode=SELECT_MODE_SINGLE, size=(10,10), enable_events=True)],
+#     ]
+#     params_right_layout = [
+#         [sg.Text("Explanation",font=titlefont)],
+#         [sg.Multiline(rule_dict[chosen_rule],size=(40,8),key='explanation')],
+#         [sg.Text(" ",size=(35,1)), sg.Button('Choose',enable_events=True)]
+#     ]
+#     params_layout = [
+#         [sg.Column(params_left_layout), sg.Column(params_right_layout)]
+#     ]
+#     params_window = sg.Window("Parameters",params_layout, font=font)
+#     while True: 
+#         params_event, params_values = params_window.read()
+#         if params_event == sg.WIN_CLOSED:
+#             break
+#         elif params_event == 'Choose':
+#             chosen_rule = params_values['selected_rule'][0]
+#             break
+#         elif params_values['selected_rule'][0] != chosen_rule:
+#             chosen_rule = params_values['selected_rule'][0]
+#             params_window.Element('explanation').Update(value=params_dict[chosen_rule])
+#     params_window.close()
+
+#     return chosen_rule
+
 main_left_layout = [
     [sg.Text("Simulation Parameters",font=titlefont)],
+    [sg.Text("Enviroment rule set:  ",font=font, size=(15,1)), sg.Multiline(default_text= "Rule1",font=font, key='chosen_env_rule',size=(10,1),no_scrollbar=True), sg.Button("Choose", key='env_rule')],
+    [sg.Text("Cell rule set:  ",font=font, size=(15,1)), sg.Multiline(default_text= "Rule1",font=font, key='chosen_cell_rule',size=(10,1),no_scrollbar=True), sg.Button("Choose", key='cell_rule')],
+    [sg.Text("Parameters:  ",font=font, size=(15,1)), sg.Button("Modify", key='params')]
+    [sg.Button("Launch simulation")]
 ]
-
 main_right_layout = [
     [sg.Canvas(size=(10,10),key="plot")],
 ]
 main_layout = [
     [sg.Column(main_left_layout), sg.Column(main_right_layout)]
 ]
-window = sg.Window("Cell simulation",main_layout, finalize=True,font=font,resizable=True)
+window = sg.Window("Cells Evolution Simulator", main_layout, finalize=True,font=font,resizable=True, location=(20,20))
+
+fig = draw_figure(window['plot'].TKCanvas, plot(None,None))
 
 def main():
     while True: 
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
-    window.close()
+        elif event == 'env_rule':
+            chosen_rule = values['chosen_env_rule']
+            chosen_rule = choose_rule(env_rules,chosen_rule, "Environment Rule Selection")
+            window.Element('chosen_env_rule').Update(value=chosen_rule)
 
+        elif event == 'cell_rule':
+            chosen_rule = values['chosen_cell_rule']
+            chosen_rule = choose_rule(cell_rules,chosen_rule, "Cell Rule Selection")
+            window.Element('chosen_cell_rule').Update(value=chosen_rule)
+            
+    window.close()
 
 if __name__ == '__main__':
     main()
-
