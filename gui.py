e5edba5b299b43a630baa53108e83a00fc44b516
flexflower <kelyancsajka@icloud.com>
2021-12-20 16:18:43 +0100

    remove try except as it serves no purpose anymore

--- gui.py
+++ gui.py
@@ -20,45 +20,42 @@ default_plotrule = config["default_plotrule"]
 
 main = MainThread(default_envrule, default_cellrule, default_plotrule)
 main.next()
+
 while True:
-    try:
-        event, values = main.window.read()
-        
-        if event == sg.WIN_CLOSED:
-            break
-        
-        if event == 'Next' or event == 'right':
-            main.next()
+    event, values = main.window.read()
+    
+    if event == sg.WIN_CLOSED:
+        break
+    
+    if event == 'Next' or event == 'right':
+        main.next()
 
-        if event == 'Launch':
-            main.launch()
-        
-        if event == 'Pause':
-            main.pause()
+    if event == 'Launch':
+        main.launch()
+    
+    if event == 'Pause':
+        main.pause()
 
-        if event == 'Parameters':
-            main.pause()
-            new_params = opt_win.change_params(main.sim) 
-            main.resume()
+    if event == 'Parameters':
+        main.pause()
+        new_params = opt_win.change_params(main.sim) 
+        main.resume()
 
-        if event == 'Environment Rule':
-            main.pause()
-            new_envrule = opt_win.choose_rule(chosen_env_rule=main.sim.envrule)
-            main.sim.update_rules(new_envrule=new_envrule)
-            main.resume()
+    if event == 'Environment Rule':
+        main.pause()
+        new_envrule = opt_win.choose_rule(chosen_env_rule=main.sim.envrule)
+        main.sim.update_rules(new_envrule=new_envrule)
+        main.resume()
 
-        if event == 'Cells Rule':
-            main.pause()
-            new_cellrule = opt_win.choose_rule(chosen_cell_rule=main.sim.cellrule)
-            main.sim.update_rules(new_cellrule=new_cellrule) 
-            main.resume()
+    if event == 'Cells Rule':
+        main.pause()
+        new_cellrule = opt_win.choose_rule(chosen_cell_rule=main.sim.cellrule)
+        main.sim.update_rules(new_cellrule=new_cellrule) 
+        main.resume()
 
-        if event == 'Plotting':
-            main.pause()
-            opt_win.plotting(main.plot_stgs)
-            main.resume()
-
-    except RuntimeError:
-        continue
+    if event == 'Plotting':
+        main.pause()
+        opt_win.plotting(main.plot_stgs)
+        main.resume()
 
 main.window.close()
